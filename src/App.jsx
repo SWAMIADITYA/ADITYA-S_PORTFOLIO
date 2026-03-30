ba1f2fd705d367e05fb4dac5ed09863d37a4b2c8
ADITYA K. SWAMI <84822278+SWAMIADITYA@users.noreply.github.com>
2026-03-30 19:03:35 +0530

    Comment out About component import
    Comment out the import statement for About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import About from './components/About';
+// import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Certificates from "./components/Certificates";
